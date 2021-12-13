143cc196fa2403c09c411ffb531c1b8246c8c4da
AnaisDiGenua <anais.digenua5@gmail.com>
2021-12-13 16:05:05 +0100

    header structure

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="logo pagina dc">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Characters</a></li>
+                    <li><a href="">Comics</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
