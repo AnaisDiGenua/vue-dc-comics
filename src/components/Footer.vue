83f8389c3ced85fee64e770d59bdf326efb64326
AnaisDiGenua <anais.digenua5@gmail.com>
2021-12-13 19:25:49 +0100

    footer-bottom structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -61,10 +61,33 @@
             <!-- footer bottom -->
             <div class="footer-bottom">
                 <div class="container">
-                    <div class="sign-up">
+                    <div class="sign-up-cta">
                     <button>Sign-up now!</button>
                     </div>
-                    <div class="social-bar"></div>
+                    <div class="social-box">
+                        <div class="follow-us-cta">
+                            <p>Follow us</p>
+                        </div>
+                        <div class="social-bar">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-facebook.png" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-facebook.png" alt="">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <img src="../assets/img/footer-facebook.png" alt="">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
             <!-- /footer bottom -->
@@ -157,9 +180,11 @@ export default {
 
         .container {
             display: flex;
+            justify-content: space-between;
+            align-items: center;
             padding: 20px 0;
 
-            .sign-up button {
+            .sign-up-cta button {
                 background-color: $darkGray;
                 color: $white;
                 font-size: .9375rem;
@@ -169,6 +194,33 @@ export default {
                 border: 2px solid $secondaryColor;
                             }
                     }
+    }
+
+    .social-box {
+        display: flex;
+        align-items: center;
+
+        .follow-us-cta {
+            margin-right: 20px;
+
+            p {
+                text-transform: uppercase;
+                font-weight: 700;
+                color: $secondaryColor;
+            }
+        }
+        .social-bar ul {
+            list-style: none;
+            
+            li {
+                display: inline-block;
+                margin: 0 10px;
+
+                a img {
+                    width: 30px;
+                }
+            }
+        }
 
     }
 
