c04fbc7892d3fa0c241de22f37426ba7927431b4
AnaisDiGenua <anais.digenua5@gmail.com>
2021-12-13 16:35:56 +0100

    header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
             <nav>
                 <ul>
                     <li><a href="">Characters</a></li>
-                    <li><a href="">Comics</a></li>
+                    <li><a href="" class="active">Comics</a></li>
                 </ul>
             </nav>
         </div>
@@ -20,6 +20,46 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang = "scss" scoped>
+@import '../assets/style/partials/variables.scss';
 
+    header {
+        height: 100px;
+
+            .container {
+                height: 100%;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+                        .logo img {
+                            width: 80px;
+                        }
+            }
+    }
+
+
+    nav {
+        text-align: right;
+
+        ul {
+            list-style: none;
+
+            li {
+                display: inline-block;
+
+                a {
+                    text-decoration: none;
+                    text-transform: uppercase;
+                    font-weight: 700;
+                    padding: 0 15px;
+                    color: $darkGray;
+
+                    &.active, &:hover {
+                        color: $secondaryColor;
+                    }
+                }
+            }
+        }
+    }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,11 @@
 <template>
-    <main></main>
+    <main>
+        <div class="container">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ea sit, nobis ipsa eum voluptatem eius totam doloribus corporis eaque natus obcaecati eos, reiciendis impedit quos iure in vitae earum?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae qui odit veritatis eaque exercitationem eligendi a voluptate maxime voluptates aliquam excepturi ullam veniam, quo aut, aperiam eveniet magnam esse harum!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quos architecto saepe dolorem. Possimus reiciendis consequatur molestias maxime ad harum ab optio! Ipsa tempora, eaque suscipit quod quisquam molestias consequuntur!
+        </div>
+    </main>
 </template>
 
 <script>
